6bc6ea52cc0aefc0894c075522ac80b9c50e6803
unknown <997850262@qq.com>
2018-08-13 19:58:38 +0800

    8.13作业

--- src/component/Power/PowerItemRight.js
+++ src/component/Power/PowerItemRight.js
@@ -7,20 +7,27 @@ export default class PowerItemRight extends Component {
     constructor(props) {
         super(props);
       }
-      handlename=(item,todoActions)=>{
-        console.log('获取名字',item.name)
-        const id=item.id;
+    //   handlename=(item,todoActions)=>{
+    //     console.log('获取名字',item.name)
+    //     const id=item.id;
+    //     todoActions.fitchimg(id)
+    //   }
+      onSelect = (todoActions) => {
+        console.log('Trigger Select');
+        const id=0;
         todoActions.fitchimg(id)
-      }
+      };
+      onExpand = () => {
+        console.log('Trigger Expand');
+      };
+    
     mapRender = (newList,todoActions)=>{
         return newList.map(item => {
             console.log(item)
             return (
-                <div onClick={()=>this.handlename(item,todoActions)}>
                     <TreeNode title={item.nick} key="0-0">
                     {this.mapRender2(newList,item,todoActions)}
                     </TreeNode>
-                </div>
             )
         })
     }
@@ -30,39 +37,31 @@ export default class PowerItemRight extends Component {
         return item.department2.map(item2 => {
             console.log(1111111111)
             return(
-                <div>
                     <TreeNode title={item2.nick} key="0-0-0">
                     {this.mapRender3(newList,item,todoActions)}
                     </TreeNode>
-                </div>
             )
         })
     }
     mapRender3 = (newList,item,todoActions)=>{
         return item.department3.map(item3 => {
             return(
-                <div>
                     <TreeNode title={item3.nick} key="0-0-0-0">
                     {this.mapRender4(newList,item,todoActions)}
                     </TreeNode>
-                </div>
             )
         })
     }
     mapRender4 = (newList,item,todoActions)=>{
         return item.department4.map(item4 => {
             return(
-                <div>
-                    <TreeNode title={item4.nick} key="0-0-0-0-0">
-                    </TreeNode>
-                </div>
+                    <TreeNode title={item4.nick} key="0-0-0-0-0" isLeaf />
+                    
             )
         })
     }
-    showimg=()=>{
-        const{Power}=this.props;
-        console.log(Power.showimg)
-        const showimg=Power.showimg;
+    showimg=(showimg)=>{
+        console.log(showimg)
         if(showimg==1)
         {
             <div>王鹏</div>
@@ -78,10 +77,15 @@ export default class PowerItemRight extends Component {
         console.log(Power.idx)
         console.log(newList)
         console.log(Power.showimg)
+        const showimg=Power.showimg;
         return(
         <div className="PowerItemRight-all">
             <div className="PowerItemRight-1">
-                <DirectoryTree>
+                <DirectoryTree
+                multiple
+                onExpand={this.onExpand}
+                onSelect={()=>this.onSelect(todoActions)}
+                >
                 {this.mapRender(newList,todoActions)}
                 {/* <TreeNode title="爱奇迪集团" key="0-0">
                     <TreeNode title="广州分公司" key="0-0-0" >
@@ -104,7 +108,7 @@ export default class PowerItemRight extends Component {
                 </DirectoryTree>
             </div>
             <div className="PowerItemRight-2">
-                {this.showimg}
+                {this.showimg(showimg)}
             </div>
         </div>
         )
