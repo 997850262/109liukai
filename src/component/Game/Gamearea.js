d505cc550860b61a4d721577812358df23ee4eec
unknown <997850262@qq.com>
2018-08-17 14:12:46 +0800

    8.17

--- src/component/Game/Gamearea.js
+++ src/component/Game/Gamearea.js
@@ -1,108 +1,93 @@
 import React, { Component } from 'react';
 import './Game.css';
-/* eslint-disable */
 
 // const KEY_RETURN = [87,65,83,68];//W A S D
 export default class Gamearea extends Component {
-  constructor(props) {
-    super(props);
-    // this.state={
+  // constructor(props) {
+  //   super(props);
+  // }
 
-    // }
+  onTouchStart=event => {
+    console.log(event);
+    console.log(event.touches);
+    this.startX = event.touches[0].clientX;
+    this.startY = event.touches[0].clientY;
+    console.log('1111111111111', this.startX);
+    console.log('1111111111111', this.startY);
   }
-  rendergamebtn=()=>{
-    const{data}=this.props;
-    let{color}=this.props
-    // console.log('渲染按钮的值',data)
-    return data.map((n,hang)=>{
-    // console.log(hang)
-    return  n.map((num,lie)=>{
-      // console.log(lie)
-      // console.log('内容',data[hang][lie])
-      if(data[hang][lie]>0){
-        if(num==4){color='green'}
-        else if(num==8){color='orange'}
-        else if(num==16){color='red'}
-        return (
-          <div className="btn1-1" style={{ background: `${color}` }}>{data[hang][lie]}</div>
-        )
-      }
-      else
-        return (
-          <div className="btn1-1"></div>
-        )
-      })
-    })
-  }
-
-  onTouchStart=(event)=>{
-  console.log(event)
-  console.log(event.touches)
-  this.startX = event.touches[0].clientX;
-  this.startY = event.touches[0].clientY;	
-  console.log('1111111111111',this.startX)
-  console.log('1111111111111',this.startY)
-  }
-  onTouchEnd=(event)=>{
-    const{todoActions}=this.props;
-  console.log(event.changedTouches)
-  this.endX = event.changedTouches[0].clientX;
-  this.endY = event.changedTouches[0].clientY;
-  console.log('22222222',this.endX)
-  console.log('22222222',this.endY)
-  var x = this.endX - this.startX;
-  var y = this.endY - this.startY;
-  console.log('33333',x)
-  console.log('33333',y)
-    if(x>0&&y>0){
-      if(x-y>0)
-      {
+  onTouchEnd=event => {
+    const { todoActions } = this.props;
+    console.log(event.changedTouches);
+    this.endX = event.changedTouches[0].clientX;
+    this.endY = event.changedTouches[0].clientY;
+    console.log('22222222', this.endX);
+    console.log('22222222', this.endY);
+    const x = this.endX - this.startX;
+    const y = this.endY - this.startY;
+    console.log('33333', x);
+    console.log('33333', y);
+    if (x > 0 && y > 0) {
+      if (x - y > 0) {
         todoActions.rightward();
-      }
-      else{
+      } else {
         todoActions.downward();
       }
-    }
-    else if(x>0&&y<0){
-      if(x+y>0)
-      {
+    } else if (x > 0 && y < 0) {
+      if (x + y > 0) {
         todoActions.rightward();
-      }
-      else{
+      } else {
         todoActions.upward();
       }
-    }
-    else if(x<0&&y>0){
-      if(x+y>0){
+    } else if (x < 0 && y > 0) {
+      if (x + y > 0) {
         todoActions.downward();
-      }
-      else{
+      } else {
         todoActions.leftward();
       }
-    }
-    else if(x<0&&y<0){
-      if(x-y>0){
+    } else if (x < 0 && y < 0) {
+      if (x - y > 0) {
         todoActions.upward();
-      }
-      else{
+      } else {
         todoActions.leftward();
       }
     }
   }
-  onTouchMove=(event)=>{
-    console.log(event.defaultPrevented)
+  onTouchMove=event => {
+    console.log(event.defaultPrevented);
     event.preventDefault();
   }
+  rendergamebtn=() => {
+    const { data } = this.props;
+    let { color } = this.props;
+    // console.log('渲染按钮的值',data)
+    return data.map((n, hang) =>
+    // console.log(hang)
+      n.map((num, lie) => {
+      // console.log(lie)
+      // console.log('内容',data[hang][lie])
+        if (data[hang][lie] > 0) {
+          if (num == 4) { color = 'green'; } else if (num == 8) { color = 'orange'; } else if (num == 16) { color = 'red'; }
+          return (
+            <div className="btn1-1" style={{ background: `${color}` }}>{data[hang][lie]}</div>
+          );
+        }
+        return (
+          <div className="btn1-1" />
+        );
+      }));
+  }
   render() {
-    const{data}=this.props;
-    console.log('游戏区域',data)
+    const { data } = this.props;
+    console.log('游戏区域', data);
     return (
-        <div className="game-all" onTouchStart={this.onTouchStart}
-         onTouchEnd={this.onTouchEnd}
-         onTouchMove={this.onTouchMove}
-         >
+      <div
+        className="game-all"
+        onTouchStart={this.onTouchStart}
+        onTouchEnd={this.onTouchEnd}
+        onTouchMove={this.onTouchMove}
+      >
         {this.rendergamebtn()}
-        </div>
+      </div>
     );
   }
 }
--- src/reducers/game.js
+++ src/reducers/game.js
@@ -46,55 +46,120 @@ function game(state = {
         console.log('向上')
         console.log(state.data)
         let newdata = state.data.slice();
-        let m=3;
-        while(m){
-            for(let i=0;i<3;i++)
-            {
-                    newdata[i].map((item,id)=>{
-                        if(newdata[i][id]==0)
-                        {
-                                let temp=newdata[i][id];
-                                newdata[i][id]=newdata[i+1][id];
-                                newdata[i+1][id]=temp;
-                        }
-                        else if(newdata[i][id]!=0&&newdata[i][id]==newdata[i+1][id])
-                        {
-                                newdata[i][id]=2*newdata[i][id];
-                                state.Score=state.Score+newdata[i][id];
-                                if(state.Score>state.bestScore)
-                                {
-                                    state.bestScore=state.Score;
-                                }
-                                console.log('加倍',newdata[i][id])
-                                newdata[i+1][id]=0;
-                                console.log('清空',newdata[i+1][id])
-                        }
-                    })
+        // let m=3;
+        // while(m){
+        //     for(let i=0;i<3;i++)
+        //     {
+        //             newdata[i].map((item,id)=>{
+        //                 if(newdata[i][id]==0)
+        //                 {
+        //                         let temp=newdata[i][id];
+        //                         newdata[i][id]=newdata[i+1][id];
+        //                         newdata[i+1][id]=temp;
+        //                 }
+        //                 else if(newdata[i][id]!=0&&newdata[i][id]==newdata[i+1][id])
+        //                 {
+        //                         newdata[i][id]=2*newdata[i][id];
+        //                         state.Score=state.Score+newdata[i][id];
+        //                         if(state.Score>state.bestScore)
+        //                         {
+        //                             state.bestScore=state.Score;
+        //                         }
+        //                         console.log('加倍',newdata[i][id])
+        //                         newdata[i+1][id]=0;
+        //                         console.log('清空',newdata[i+1][id])
+        //                 }
+        //             })
 
-                    // for(let j=0;j<4;j++)
-                    // {
-                    //     let b=0;
-                    //     if(newdata[i][j]==0)
-                    //     {
-                    //         b=0;
-                    //             let temp=newdata[i][j];
-                    //             newdata[i][j]=newdata[i+1][j];
-                    //             newdata[i+1][j]=temp;
-                    //     }
-                    //     else if(newdata[i][j]!=0&&newdata[i][j]==newdata[i+1][j])
-                    //     {
-                    //             newdata[i][j]=2*newdata[i][j];
-                    //             state.Score=state.Score+newdata[i][j];
-                    //             console.log('加倍',newdata[i][j])
-                    //             newdata[i+1][j]=0;
-                    //             console.log('清空',newdata[i+1][j])
-                    //             b=1; 
-                    //         if(b==1) break;
-                    //     }
-                    // }
+        //             // for(let j=0;j<4;j++)
+        //             // {
+        //             //     let b=0;
+        //             //     if(newdata[i][j]==0)
+        //             //     {
+        //             //         b=0;
+        //             //             let temp=newdata[i][j];
+        //             //             newdata[i][j]=newdata[i+1][j];
+        //             //             newdata[i+1][j]=temp;
+        //             //     }
+        //             //     else if(newdata[i][j]!=0&&newdata[i][j]==newdata[i+1][j])
+        //             //     {
+        //             //             newdata[i][j]=2*newdata[i][j];
+        //             //             state.Score=state.Score+newdata[i][j];
+        //             //             console.log('加倍',newdata[i][j])
+        //             //             newdata[i+1][j]=0;
+        //             //             console.log('清空',newdata[i+1][j])
+        //             //             b=1; 
+        //             //         if(b==1) break;
+        //             //     }
+        //             // }
+        //     }
+        //     m--;
+        // }
+        let flag=0;
+        newdata[0].map((Item,idx)=>{
+            if(newdata[0][idx]==0&&newdata[1][idx]>0){
+                let temp=newdata[0][idx];
+                newdata[0][idx]=newdata[1][idx];
+                newdata[1][idx]=temp;
             }
-            m--;
-        }
+            if(newdata[0][idx]>0&&newdata[0][idx]==newdata[1][idx]){
+                newdata[0][idx]=2*newdata[0][idx];
+                state.Score=state.Score+newdata[0][idx];
+                console.log('加倍',newdata[0][idx])
+                newdata[1][idx]=0;
+            }
+
+        })
+        newdata[2].map((Item,idx)=>{
+                if(newdata[2][idx]==0&&newdata[3][idx]>0){
+                    let temp=newdata[2][idx];
+                    newdata[2][idx]=newdata[3][idx];
+                    newdata[3][idx]=temp;
+                }
+                if(newdata[1][idx]!=newdata[2][idx]){
+                    if(newdata[2][idx]>0&&newdata[2][idx]==newdata[3][idx]){
+                        newdata[2][idx]=2*newdata[2][idx];
+                        state.Score=state.Score+newdata[2][idx];
+                        console.log('加倍',newdata[2][idx])
+                        newdata[3][idx]=0;
+                        flag=1
+                    }
+                }
+        })
+        newdata[1].map((Item,idx)=>{
+            if(newdata[1][idx]==0&&newdata[2][idx]>0){
+                let temp=newdata[1][idx];
+                newdata[1][idx]=newdata[2][idx];
+                newdata[2][idx]=temp;
+            }
+            if(newdata[0][idx]>0){
+                if(flag==0){
+                    if(newdata[1][idx]>0&&newdata[1][idx]==newdata[2][idx]){
+                        newdata[1][idx]=2*newdata[1][idx];
+                        state.Score=state.Score+newdata[1][idx];
+                        console.log('加倍',newdata[1][idx])
+                        newdata[2][idx]=0;
+                        flag=1;
+                    }
+                }
+
+            }
+        })
+        newdata[0].map((Item,idx)=>{
+            if(newdata[0][idx]==0&&newdata[1][idx]>0){
+                let temp=newdata[0][idx];
+                newdata[0][idx]=newdata[1][idx];
+                newdata[1][idx]=temp;
+            }
+            if(flag==0){
+                if(newdata[0][idx]>0&&newdata[0][idx]==newdata[1][idx]){
+                    newdata[0][idx]=2*newdata[0][idx];
+                    state.Score=state.Score+newdata[0][idx];
+                    console.log('加倍',newdata[0][idx])
+                    newdata[1][idx]=0;
+                }
+            }
+        })
         return{...state,
             data:newdata
         }
